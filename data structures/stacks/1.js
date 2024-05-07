55b9927d6a404f14b9a63581cbf6b78511ebbfd3
dboatengg <dicksonboateng67@gmail.com>
2024-05-07 02:18:57 +0000

    Complete linked-list

---
+++ data structures/stacks/1.js
@@ -0,0 +1,7 @@
+/**
+ * * Stack
+ * Stack is a data structure where we keep adding things sequentially.
+ * We add/remove items from the top/end. This is known as the LIFO system.
+ * LIFO ---> Last In First Out 
+ */
+
